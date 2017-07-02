13070d5836ff5c1fb911cde652d0124385074b1d
Heshan Molligoda <heshanmolligoda@Heshans-MacBook-Pro.local>
2017-07-02 22:38:25 +0530

    second Commit

---
+++ data.py
@@ -0,0 +1,32 @@
+def Articles():
+	articles = [
+		{
+			'id' : 1,
+			'title':'Article One',
+			'body': 'this is the body',
+			'author': 'name',
+			'date': '04-08-2017'
+
+
+		},
+		{
+			'id' : 2,
+			'title':'Article two',
+			'body': 'this is the body',
+			'author': 'name1',
+			'date': '04-08-2017'
+
+
+		},
+		{
+			'id' : 3,
+			'title':'Article One',
+			'body': 'this is the body',
+			'author': 'name',
+			'date': '04-08-2017'
+
+
+		}
+	]
+
+	return articles
